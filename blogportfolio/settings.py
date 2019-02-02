ffbda4524ee26ae03ea588196128ef6fe3e5dc67
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-02-02 12:46:13 +0000

    Added local sett

--- blogportfolio/settings.py
+++ blogportfolio/settings.py
@@ -186,6 +186,10 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 
